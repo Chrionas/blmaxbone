921235da87085dc457be0fcb12ad9ab6bb345984
Jonas <13248474>
2022-08-17 14:11:35 +0800

    阿

--- blmaxbones/__init__.py
+++ blmaxbones/__init__.py
@@ -3,7 +3,7 @@ bl_info = {
     "description": "Copy animation between different armature by bone constrain",
     "author": "Jonas",
     "version": (1,0,1),
-    "blender": (2, 83, 0),
+    "blender": (2, 93, 0),
     "location": "View 3D > Toolshelf",
     "doc_url": "",
     "tracker_url": "",
